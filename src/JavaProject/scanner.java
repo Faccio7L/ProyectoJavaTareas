1937a2420f2cdd76faf2186973838a1b3e19452a
Faccio7L <Santifaccio123@gmail.com>
2026-04-01 10:52:28 -0300

    comentarios

--- src/JavaProject/scanner.java
+++ src/JavaProject/scanner.java
@@ -2,8 +2,8 @@ package JavaProject;
 import java.util.Scanner;
 
 public class scanner {
-    private static Scanner sc = new Scanner(System.in);
-
+    private static Scanner sc = new Scanner(System.in); 
+ // como es static se crea el objeto al invocar cualquier metoodo de la clase.
     public static int InicializarScanner() {
         int x = sc.nextInt();
         sc.nextLine(); 
